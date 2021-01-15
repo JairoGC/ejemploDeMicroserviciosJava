312adca0269837cc71aa0ee48a22e6ee9d393d85
Jairo Galvez <jairo.jggc@gmail.com>
2021-01-14 19:40:07 -0500

    usando cliente  Feign REST

--- currency-conversion-service/src/main/java/com/jailux/currencyconversionservice/CurrencyConversionControlador.java
+++ currency-conversion-service/src/main/java/com/jailux/currencyconversionservice/CurrencyConversionControlador.java
@@ -3,6 +3,7 @@ package com.jailux.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionControlador {
 
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calcularCurrencyConversion(
 			@PathVariable String from,
@@ -26,6 +30,23 @@ public class CurrencyConversionControlador {
 				uriVariables);
 		
 		CurrencyConversion currencyConversion = responseEntity.getBody();
+		
+		return new CurrencyConversion(
+				currencyConversion.getId(), from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()),
+				currencyConversion.getEnvironment()
+				);
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calcularCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversion(
 				currencyConversion.getId(), from, to, quantity, 
