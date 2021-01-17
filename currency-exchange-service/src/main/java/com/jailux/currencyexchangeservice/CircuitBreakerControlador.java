2b1e8a562bce0e393e17d74a2e78f6b020a02a3d
Jairo Galvez <jairo.jggc@gmail.com>
2021-01-17 11:46:09 -0500

    renombrando metodos

--- currency-exchange-service/src/main/java/com/jailux/currencyexchangeservice/CircuitBreakerControlador.java
+++ currency-exchange-service/src/main/java/com/jailux/currencyexchangeservice/CircuitBreakerControlador.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -18,7 +19,7 @@ public class CircuitBreakerControlador {
 	
 	@GetMapping("/ejemplo-api")
 	@Retry(name="ejemplo-api", fallbackMethod = "hardcodeResponse")
-	public String sampleApi() {
+	public String respuestaPredeterminadaEnCasoDeError() {
 		logger.info("solicitando api con error");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:3848/error", String.class);
 		return forEntity.getBody();
@@ -26,7 +27,7 @@ public class CircuitBreakerControlador {
 	
 	@GetMapping("/ejemplo-api-circuit")
 	@CircuitBreaker(name="default", fallbackMethod = "hardcodeResponse")
-	public String sampleApiCircuit() {
+	public String reintentarEnUnTiempoCadaVezMayor() {
 		logger.info("solicitando api con error");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:3848/error", String.class);
 		return forEntity.getBody();
@@ -34,8 +35,13 @@ public class CircuitBreakerControlador {
 	
 	@GetMapping("/ejemplo-api-rate")
 	@RateLimiter(name="default")
-	public String sampleApiRate() {
-		logger.info("solicitando api con error");
+	public String limiteDeSolicitudesPorSegundos() {
+		return "llamada exitosa";
+	}
+	
+	@GetMapping("/ejemplo-api-Bulkhead")
+	@Bulkhead(name="default")
+	public String llamadasSimultaneas() {
 		return "llamada exitosa";
 	}
 	
